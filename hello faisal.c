a3b9022ed6fc07903977aa7cf5e89dff949778e5
Faisal <dwarkaiit15@gmail.com>
2021-07-09 10:53:35 +0530

    first commit

---
+++ hello faisal.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    printf("Hello Faisal This is starting c programming\n");
+    printf("Hello world");
+
+    return 0;
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    printf("Hello Faisal This is starting c programming");
+    printf("Hello world");
+
+    return 0;
+}
